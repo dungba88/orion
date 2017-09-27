562aae3ddeb159ab15c29d740fb4759ea6699693
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-09-27 20:03:26 +0800

    change version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='orion',
-      version='0.1',
+      version='1.0',
       description='Framework for creating trigger-based web services',
       author='Anh Dung Bui',
       author_email='dungba.sg@gmail.com',
