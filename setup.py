311cafaaed47669c25fb72d3fa3dddc5f35dd394
Dzung Bui <griever@Dzungs-MacBook-Pro.local>
2017-09-27 19:43:50 +0800

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+"""Setup file"""
+
+from distutils.core import setup
+
+setup(name='orion',
+      version='0.1',
+      description='Framework for creating trigger-based web services',
+      author='Anh Dung Bui',
+      author_email='dungba.sg@gmail.com',
+      packages=['orion'])
